4dc578b9b7d35afb399c088bb1140f4fcf197772
BrunoRezende96 <brunorezende201611@gmail.com>
2026-05-28 18:30:12 -0300

    Refeito as variaveis do convex

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,11 @@ import { ConvexReactClient } from "convex/react";
 import "./index.css";
 import App from "./App";
 
-const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+// const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+
+const convex = new ConvexReactClient(
+  "https://academic-dolphin-985.convex.cloud"
+);
 
 createRoot(document.getElementById("root")!).render(
   <ConvexAuthProvider client={convex}>
